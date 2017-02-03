04120fc78eb0480b75e4f6b7de326bffdd048e9f
William Woodruff <william@tuffbizz.com>
2017-02-03 15:33:27 -0500

    kbsecret: Bump kbsecret to 0.0.2.

--- lib/kbsecret.rb
+++ lib/kbsecret.rb
@@ -8,7 +8,7 @@ require_relative "kbsecret/session"
 # The primary namespace for kbsecret.
 module KBSecret
   # kbsecret's current version
-  VERSION = "0.0.1".freeze
+  VERSION = "0.0.2".freeze
 
   # fail very early if the user doesn't have keybase and KBFS running
   raise Keybase::KeybaseNotRunningError unless Keybase.running?
