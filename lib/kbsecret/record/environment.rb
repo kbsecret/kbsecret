88e5dc5483b996b6639ca31dfedf1e1d28ce63da
William Woodruff <william@tuffbizz.com>
2017-02-03 00:33:22 -0500

    record/environment: Shell-escape value and variable.

--- lib/kbsecret/record/environment.rb
+++ lib/kbsecret/record/environment.rb
@@ -1,8 +1,13 @@
+require "shellwords"
+
 module KBSecret
   module Record
     class Environment < Abstract
       def initialize(session, label, variable, value)
         super(session, label)
+
+        value = value.shellescape
+        variable = variable.shellescape
 
         @data = {
           environment: {
@@ -31,7 +36,7 @@ module KBSecret
       end
 
       def to_assignment
-        "#{variable}='#{value}'"
+        "#{variable}=#{value}"
       end
 
       def to_export
