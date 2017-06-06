aec757bf26fed7af3a331106dc7a2b38696d20b9
William Woodruff <william@tuffbizz.com>
2017-06-05 23:23:58 -0400

    cli: Add CLI.slop for abstraction of Slop.parse.
    kbsecret's utilities repeat the same Slop.parse incantation over and over,
    so this provides a nice abstraction.

--- lib/kbsecret/cli.rb
+++ lib/kbsecret/cli.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
 require "pastel"
+require "slop"
 
 module KBSecret
   # An encapsulation of useful methods for kbsecret's CLI.
   module CLI
     class << self
       # The pastel object used to generate colorful output.
+      # @api private
       PASTEL = Pastel.new
 
       # Print an error message and terminate.
@@ -26,6 +28,28 @@ module KBSecret
         die "Unknown session: '#{sess_label}'." unless Config.session? sess_label
 
         Session.new label: sess_label
+      end
+
+      # Parse arguments for a kbsecret utility, adding some default options for
+      #  introspection and help output.
+      # @param cmds [Array<String>] additional commands to print in `--introspect-flags`
+      # @param errors [Boolean] whether or not to produce Slop errors
+      # @return [Slop::Result] the result of argument parsing
+      def slop(cmds: [], errors: false)
+        Slop.parse suppress_errors: !errors do |o|
+          yield o
+
+          o.on "-h", "--help" do
+            puts o
+            exit
+          end
+
+          o.on "--introspect-flags" do
+            comp = o.options.flat_map(&:flags) + cmds
+            puts comp.join "\n"
+            exit
+          end
+        end
       end
     end
   end
