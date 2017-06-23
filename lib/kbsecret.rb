335c7f78510efdd480d46328fcf94339df70c9a9
William Woodruff <william@tuffbizz.com>
2017-06-23 18:22:29 -0400

    kbsecret: Remove duplicated VERSION constant.
    Bump to 0.5.2 to reflect fix.

--- lib/kbsecret.rb
+++ lib/kbsecret.rb
@@ -2,6 +2,7 @@
 
 require "keybase"
 
+require_relative "version"
 require_relative "kbsecret/config"
 require_relative "kbsecret/exceptions"
 require_relative "kbsecret/record"
@@ -10,9 +11,6 @@ require_relative "kbsecret/cli"
 
 # The primary namespace for kbsecret.
 module KBSecret
-  # kbsecret's current version
-  VERSION = "0.5.0"
-
   # fail very early if the user doesn't have keybase and KBFS running
   raise Keybase::KeybaseNotRunningError unless Keybase.running?
   raise Keybase::KBFSNotRunningError unless Dir.exist?(Config[:mount])
