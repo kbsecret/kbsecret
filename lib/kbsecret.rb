99df93541ceda1fee4ef5ece9bdb6d07dbf8bc2d
William Woodruff <william@tuffbizz.com>
2017-10-24 19:48:06 -0400

    lib: More resilient KBFS test
    Ensures that KBFS is actually mounted, instead of just checking for the
    existence of the mountpoint as a directory. This should prevent users
    from clobbering their actual KBFS mount.
    
    Bumps keybase-unofficial to 0.8 to support these changes.
    
    Fixes #42

--- lib/kbsecret.rb
+++ lib/kbsecret.rb
@@ -12,7 +12,7 @@ require_relative "kbsecret/cli"
 
 # The primary namespace for {KBSecret}.
 module KBSecret
-  # fail very early if the user doesn't have keybase and KBFS running
+  # fail very early if the user doesn't have Keybase running and KBFS mounted
   raise Keybase::Local::Exceptions::KeybaseNotRunningError unless Keybase::Local.running?
-  raise Keybase::Local::Exceptions::KBFSNotRunningError unless Dir.exist?(Config[:mount])
+  raise Keybase::Local::Exceptions::KBFSNotRunningError unless Keybase::Local::KBFS.mounted?
 end
