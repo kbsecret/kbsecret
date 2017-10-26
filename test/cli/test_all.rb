09544a81cf13e6d9fe33c0b129561f7b579b1016
dheles <drew@codhicitta.com>
2017-10-25 22:37:45 -0400

    test: Add CLI tests - update test_all

--- test/cli/test_all.rb
+++ test/cli/test_all.rb
@@ -1,4 +1,12 @@
+# frozen_string_literal: true
+
 # NOTE: run the test suite with: $ bundle exec ruby -I lib:test test/cli/test_all.rb -v
 require_relative "test_kbsecret"
 require_relative "test_new"
 require_relative "test_login"
+require_relative "test_env"
+require_relative "test_todo"
+require_relative "test_dump_fields"
+require_relative "test_generators"
+require_relative "test_generator"
+require_relative "test_list"
