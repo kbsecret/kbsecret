3a989221d78745f6853cbd412062d1b9484cfc79
William Woodruff <william@yossarian.net>
2018-03-16 19:19:39 -0400

    test/cli: Add `kbsecret conf` flag tests

--- test/cli/test_conf.rb
+++ test/cli/test_conf.rb
@@ -12,7 +12,7 @@ class CLIConfTest < Minitest::Test
     setup_aruba
   end
 
-  def test_conf
+  def test_conf_opens_editor
     # with EDITOR unset, `kbsecret conf` should produce an error message
     delete_environment_variable "EDITOR"
     kbsecret "conf", interactive: false do |_, stderr|
@@ -23,6 +23,20 @@ class CLIConfTest < Minitest::Test
     set_environment_variable "EDITOR", "cat"
     kbsecret "conf", interactive: false do |stdout, _|
       assert_match(/:mount:/, stdout)
+    end
+  end
+
+  def test_conf_prints_conf_directory
+    kbsecret "conf", "-d", interactive: false do |stdout, _|
+      conf_dir = stdout.chomp
+      assert Dir.exist?(conf_dir)
+    end
+  end
+
+  def test_conf_prints_record_directory
+    kbsecret "conf", "-v", interactive: false do |stdout, _|
+      record_dir = stdout.chomp
+      assert Dir.exist?(record_dir)
     end
   end
 end
