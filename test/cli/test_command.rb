8f04d8fd6febeeddc3e8383268cf31e78609535c
William Woodruff <william@yossarian.net>
2018-04-01 18:50:33 -0400

    test_command: Add tests for external commands

--- test/cli/test_command.rb
+++ test/cli/test_command.rb
@@ -19,18 +19,41 @@ class KBSecretCommandTest < Minitest::Test
     # the number of command classes is the same as the number of command names
     assert_equal command_classes.size, command_names.size
 
-    # each member of command_names is a String and has a corresponding class in command_classes
+    # each member of command_names is a String, has a corresponding class in command_classes,
+    # and satisfies internal?
     command_names.each do |cmd_name|
       assert_instance_of String, cmd_name
       assert_instance_of Class, KBSecret::CLI::Command.internal_command_for(cmd_name)
+      assert KBSecret::CLI::Command.internal?(cmd_name)
     end
 
     # each member of command_classes is a Class, has a command_name in command_names,
-    # and is a subclass of KBSecret::CLI::Command::Abstract
+    # satisfies internal? with its command_name, and is a subclass of
+    # KBSecret::CLI::Command::Abstract
     command_classes.each do |klass|
       assert_instance_of Class, klass
       assert_includes command_names, klass.command_name
+      assert KBSecret::CLI::Command.internal?(klass.command_name)
       assert_equal KBSecret::CLI::Command::Abstract, klass.superclass
+    end
+  end
+
+  def test_external_command_paths_and_names
+    command_paths = KBSecret::CLI::Command.external_command_paths
+    command_names = KBSecret::CLI::Command.external_command_names
+
+    # command_paths and command_names are both arrays, although both may be empty
+    assert_instance_of Array, command_paths
+    assert_instance_of Array, command_names
+
+    # each member of command_paths exists on disk
+    command_paths.each do |path|
+      assert File.file?(path)
+    end
+
+    # each member of command_names satisfies external?
+    command_names.each do |cmd_name|
+      assert KBSecret::CLI::Command.external?(cmd_name)
     end
   end
 end
