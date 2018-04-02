ffb2b34ccd961c35f4d0f8985e75106d8f4e5f0a
William Woodruff <william@yossarian.net>
2018-04-02 18:47:06 -0400

    test/cli: Add tests for `kbsecret commands`

---
+++ test/cli/command/test_commands.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+require "helpers"
+
+# Tests for KBSecret::CLI::Command::Commands
+class KBSecretCommandCommandsTest < Minitest::Test
+  include Helpers
+  include Helpers::CLI
+
+  def test_commands_help
+    commands_helps = [
+      %w[help --help],
+      %w[help -h],
+      %w[help help],
+    ]
+
+    commands_helps.each do |commands_help|
+      stdout, = kbsecret(*commands_help)
+      assert_match(/Usage:/, stdout)
+    end
+  end
+
+  def test_commands_lists_all_commands
+    stdout, = kbsecret "commands"
+
+    stdout.lines.each do |command|
+      command.chomp!
+      assert KBSecret::CLI::Command.all_command_names.include?(command)
+    end
+  end
+
+  def test_commands_lists_all_external_commands
+    stdout, = kbsecret "commands", "-e"
+
+    stdout.lines.each do |command|
+      command.chomp!
+      assert KBSecret::CLI::Command.external?(command)
+    end
+  end
+
+  def test_commands_lists_all_internal_commands
+    stdout, = kbsecret "commands", "-i"
+
+    stdout.lines.each do |command|
+      command.chomp!
+      assert KBSecret::CLI::Command.internal?(command)
+    end
+  end
+end
