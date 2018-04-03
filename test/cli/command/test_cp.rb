d9d2860f82b3a4b4848bd5c74d492bf16c77a28a
William Woodruff <william@yossarian.net>
2018-04-03 17:54:12 -0400

    test/cli: Fill in `kbsecret cp` tests

--- test/cli/command/test_cp.rb
+++ test/cli/command/test_cp.rb
@@ -45,12 +45,24 @@ class KBSecretCommandCpTest < Minitest::Test
     kbsecret "rm", "test-cp-same-session"
   end
 
-  def test_cp_no_such_records
-    skip
+  def test_cp_no_such_record
+    kbsecret "session", "new", "cp-test-no-such-records", "-r", "cp-test-no-such-records"
+
+    _, stderr = kbsecret "cp", "cp-test-no-such-records", "default", "this-does-not-exist"
+
+    assert_match "No such record(s)", stderr
+  ensure
+    kbsecret "session", "rm", "-d", "cp-test-no-such-records"
   end
 
   def test_cp_fails_on_nonexistent_session
-    skip
+    _, stderr = kbsecret "cp", "cp-test-nonexistent-session", "default", "whatever"
+
+    assert_match "Unknown session", stderr
+
+    _, stderr = kbsecret "cp", "default", "cp-test-nonexistent-session", "whatever"
+
+    assert_match "Unknown session", stderr
   end
 
   def test_cp_copy
@@ -72,7 +84,26 @@ class KBSecretCommandCpTest < Minitest::Test
   end
 
   def test_cp_copy_multiple
-    skip
+    kbsecret "new", "login", "test-cp-copy-multi1", input: "foo\nbar\n"
+    kbsecret "new", "login", "test-cp-copy-multi2", input: "baz\nquux\n"
+    kbsecret "session", "new", "cp-test-session-multi", "-r", "cp-test-session-multi"
+
+    # the records aren't in the dest session yet, but are in the source session
+    assert KBSecret::Session[:default].record?("test-cp-copy-multi2")
+    assert KBSecret::Session[:default].record?("test-cp-copy-multi2")
+    refute KBSecret::Session["cp-test-session-multi"].record?("test-cp-copy-multi1")
+    refute KBSecret::Session["cp-test-session-multi"].record?("test-cp-copy-multi1")
+
+    kbsecret "cp", "default", "cp-test-session-multi", "test-cp-copy-multi1", "test-cp-copy-multi2"
+
+    # the records are now in both sessions
+    assert KBSecret::Session[:default].record?("test-cp-copy-multi2")
+    assert KBSecret::Session[:default].record?("test-cp-copy-multi2")
+    assert KBSecret::Session["cp-test-session-multi"].record?("test-cp-copy-multi1")
+    assert KBSecret::Session["cp-test-session-multi"].record?("test-cp-copy-multi1")
+  ensure
+    kbsecret "rm", "test-cp-copy-multi1", "test-cp-copy-multi2"
+    kbsecret "session", "rm", "-d", "cp-test-session-multi"
   end
 
   def test_cp_move
@@ -93,7 +124,26 @@ class KBSecretCommandCpTest < Minitest::Test
   end
 
   def test_cp_move_multiple
-    skip
+    kbsecret "new", "login", "test-cp-move-multi1", input: "foo\nbar\n"
+    kbsecret "new", "login", "test-cp-move-multi2", input: "baz\nquux\n"
+    kbsecret "session", "new", "cp-test-session-multi", "-r", "cp-test-session-multi"
+
+    # the records aren't in the dest session yet, but are in the source session
+    assert KBSecret::Session[:default].record?("test-cp-move-multi2")
+    assert KBSecret::Session[:default].record?("test-cp-move-multi2")
+    refute KBSecret::Session["cp-test-session-multi"].record?("test-cp-move-multi1")
+    refute KBSecret::Session["cp-test-session-multi"].record?("test-cp-move-multi1")
+
+    kbsecret "cp", "-m", "default", "cp-test-session-multi", "test-cp-move-multi1", "test-cp-move-multi2"
+
+    # the records are now in the dest session, and no longer in the source
+    refute KBSecret::Session[:default].record?("test-cp-move-multi2")
+    refute KBSecret::Session[:default].record?("test-cp-move-multi2")
+    assert KBSecret::Session["cp-test-session-multi"].record?("test-cp-move-multi1")
+    assert KBSecret::Session["cp-test-session-multi"].record?("test-cp-move-multi1")
+  ensure
+    kbsecret "rm", "test-cp-move-multi1", "test-cp-move-multi2"
+    kbsecret "session", "rm", "-d", "cp-test-session-multi"
   end
 
   def test_cp_fails_on_overwrite
@@ -107,6 +157,23 @@ class KBSecretCommandCpTest < Minitest::Test
   end
 
   def test_cp_force_overwrite
-    skip
+    kbsecret "session", "new", "cp-test-session-overwrite", "-r", "cp-test-session-overwrite"
+    kbsecret "new", "login", "test-cp-force-overwrite", input: "foo\nbar\n"
+    kbsecret "new", "-s", "cp-test-session-overwrite", "login", "test-cp-force-overwrite", input: "baz\nquux\n"
+
+    login = KBSecret::Session[:default]["test-cp-force-overwrite"]
+
+    assert_equal "foo", login.username
+    assert_equal "bar", login.password
+
+    kbsecret "cp", "-f", "cp-test-session-overwrite", "default", "test-cp-force-overwrite"
+
+    login = KBSecret::Session[:default]["test-cp-force-overwrite"]
+
+    assert_equal "baz", login.username
+    assert_equal "quux", login.password
+  ensure
+    kbsecret "rm", "test-cp-force-overwrite"
+    kbsecret "session", "rm", "-d", "cp-test-session-overwrite"
   end
 end
